5e9e131a5277e00418d5096c1fd1f6532ab69080
Marta <mstak@softserveinc.com>
2018-07-04 12:56:47 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ const fFor = fibonacciFor(aFor);
 alert (fFor);
 
 function fibonacciFor (aFor) {
-    if (aFor <= 1) return 1;
+    if (aFor <= 2) return 1;
     let a = 1;
     let b = 1;
     console.log(a);
     console.log(b);
     let fib;
-    for (let i = 0; i <= aFor; i++) {
+    for (let i = 3; i <= aFor; i++) {
          fib = a + b;
          a = b;
          b = fib;
@@ -25,16 +25,14 @@ const fArr = fibonacciArr(aArr);
 alert (fArr);
 
 function fibonacciArr (aArr) {
-    if (aFor <= 1) return 1;
     let fibArr = [1, 1];
-    for (let i = 0; i <= aArr; i++) {
+    for (let i = 2; i < aArr; i++) {
         fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length-2]);
     }
     for (let i = 0; i < fibArr.length; i++) {
         console.log(fibArr[i]);
     }
-    let l = fibArr.length-1;
-    return fibArr[l];
+    return fibArr[fibArr.length-1];
 }
 
 const aRec = parseFloat(prompt('Please enter a number:'));
@@ -43,12 +41,19 @@ const fRec = fibonacciRec(aRec);
 alert (fRec);
 
 function fibonacciRec (aRec) {
-    if (aRec <= 1) return 1;
-    let fR = fibonacciRec(aRec - 1) + fibonacciRec(aRec - 2);
-    console.log(fR);
-    return fR;
+    if (aRec <= 2) return 1;
+    return fibonacciRec(aRec - 1) + fibonacciRec(aRec - 2);
 }
 
 
+const aMath = parseFloat(prompt('Please enter a number:'));
 
+const fMath = fibonacciMath(aMath);
+alert (fMath);
+
+function fibonacciMath (aMath) {
+    if (aRec <= 2) return 1;
+    console.log(Math.round((Math.pow((1 + Math.sqrt(5)) / 2, aMath) - Math.pow(-2 / (1 + Math.sqrt(5)), aMath)) / Math.sqrt(5)));
+    return Math.round((Math.pow((1 + Math.sqrt(5)) / 2, aMath) - Math.pow(-2 / (1 + Math.sqrt(5)), aMath)) / Math.sqrt(5)) ;
+}
 
